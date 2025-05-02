b70d5842aec7e837d614f3e3adde083743f17420
Moustafa-33 <158188840+Moustafa-33@users.noreply.github.com>
2025-05-02 13:15:51 +0100

    Update extensions.py

--- models/extensions.py
+++ models/extensions.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-# 🔁 Shared instances used across the app
+#  Shared instances used across the app
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
